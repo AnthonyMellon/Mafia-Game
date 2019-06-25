1db58b44b98706dd02c03eb3a55d9e69fdd7a4b6
Anthony <Anthonymellon@yahoo.com>
2019-06-25 18:04:12 +1200

    Mafia members now know of other Mafia members

--- Town of Salem/Town of Salem/Program.cs
+++ Town of Salem/Town of Salem/Program.cs
@@ -12,16 +12,20 @@ namespace Town_of_Salem
         public struct Player
         {
             public string Name;
+            public string affiliation;
             public string role;
             public string personality;
             public bool alive;
             public int votes; //Number of votes their vote is worth
             public int[] suspicions; //Range from 0 to 100. 0 they trust someone with their lives. 100 they'll try to lynch them every round. 50 is neutral.
-            public int[] roleThoughts; //What they think each players role is
+            public string[] roleThoughts; //What they think each players role is
         };
 
-        public static string[] personalities = {"Timid", "Hostile", "Passive"};
-        public static string[] roles = {"Innocent", "Mafia Killer"};        
+        public static string[] personalities = { "Timid", "Hostile", "Passive" };
+        public static string[] Affiliations = {"Town", "Mafia", "Self"};
+        public static string[] TownRoles = { "Innocent"};
+        public static string[] MafiaRoles = {"Killer", "GodFather"};
+        public static string[] SelfRoles = {"Serial Killer"};
 
         static void GameLoop()
         {
@@ -37,33 +41,60 @@ namespace Town_of_Salem
             int LastNamesTotal = GetFileLength("Last Names.txt");
 
 
-            for (int currentPlayer = 0; currentPlayer < players.Length; currentPlayer++)
-            {                
+            for (int currentPlayer = 0; currentPlayer < players.Length; currentPlayer++) //Set up all players settings that dont need knowledge of other players
+            {       
                 players[currentPlayer].Name = $"{ReadLine("First Names.txt", rand.Next(FirstNamesTotal))} {ReadLine("Last Names.txt", rand.Next(LastNamesTotal))}";
-                players[currentPlayer].role = roles[rand.Next(roles.Length)];
+                players[currentPlayer].affiliation = Affiliations[rand.Next(Affiliations.Length)];                
                 players[currentPlayer].personality = personalities[rand.Next(personalities.Length)];
                 players[currentPlayer].votes = 1;
                 players[currentPlayer].alive = true;
                 players[currentPlayer].suspicions = new int[playerCount];
+                players[currentPlayer].roleThoughts = new string[playerCount];
 
+                switch(players[currentPlayer].affiliation) //Set role based on affiliation
+                {
+                    case "Town":
+                        players[currentPlayer].role = TownRoles[rand.Next(TownRoles.Length)]; 
+                        break;
+                    case "Mafia":
+                        players[currentPlayer].role = MafiaRoles[rand.Next(MafiaRoles.Length)];
+                        break;
+                    case "Self":
+                        players[currentPlayer].role = SelfRoles[rand.Next(SelfRoles.Length)];
+                        break;
+                }
+                
+                
+            } //First character loop END
+
+            for (int currentPlayer = 0; currentPlayer < players.Length; currentPlayer++) //Going through each player again to set stats that need knowledge of other players
+            {
                 for (int targetPlayer = 0; targetPlayer < players[currentPlayer].suspicions.Length; targetPlayer++) //Set suspicions
                 {
                     if (currentPlayer == targetPlayer)
                     {
                         players[currentPlayer].suspicions[targetPlayer] = 0;
-                    }
-                    else if (players[currentPlayer].role == "Mafia Killer" && players[targetPlayer].role == "Mafia Killer")
-                    {
-                        players[currentPlayer].suspicions[targetPlayer] = 0;
+                        players[currentPlayer].roleThoughts[targetPlayer] = players[currentPlayer].role;
                     }
                     else
                     {
-                        players[currentPlayer].suspicions[targetPlayer] = 50;
+                        if (players[currentPlayer].affiliation == "Mafia" && (players[currentPlayer].affiliation == players[targetPlayer].affiliation))
+                        {
+                            players[currentPlayer].suspicions[targetPlayer] = 0;
+                            players[currentPlayer].roleThoughts[targetPlayer] = players[targetPlayer].role;
+                            Console.WriteLine($"Im {players[currentPlayer].Name}, im affiliated with the {players[currentPlayer].affiliation} and my role is {players[currentPlayer].role}");
+                            
+                        }
+                        else
+                        {
+                            players[currentPlayer].suspicions[targetPlayer] = 50;
+                        }
+                        
                     }
+                                    
+                } //Set suspicions END
 
-                }
-
-            }
+            } //Second charatcer loop END
 
         }
 
@@ -90,15 +121,16 @@ namespace Town_of_Salem
             {
                 reader.ReadLine();
                 length++;
+
             } while (!reader.EndOfStream);
+
             reader.Close();
             return length;
         }
 
         static void Main()
         {
-            Initialisation();
-            Console.ReadLine();
+            Initialisation();            
         }
     }
 }
